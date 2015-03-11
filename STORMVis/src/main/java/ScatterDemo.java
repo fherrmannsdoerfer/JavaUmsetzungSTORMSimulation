d8e54e72b84578cea7dd907d011b1f6c089aca27
Maximilian Scheurer <maximilianscheurer@MaximiliansiMac.fritz.box>
2015-03-11 13:56:42 +0100

    jzy start

---
+++ STORMVis/src/main/java/ScatterDemo.java
@@ -0,0 +1,99 @@
+import java.awt.event.MouseWheelEvent;
+import java.util.Random;
+
+import org.jzy3d.analysis.AbstractAnalysis;
+import org.jzy3d.analysis.AnalysisLauncher;
+import org.jzy3d.chart.factories.AWTChartComponentFactory;
+import org.jzy3d.colors.Color;
+import org.jzy3d.maths.BoundingBox3d;
+import org.jzy3d.maths.Coord3d;
+import org.jzy3d.maths.Scale;
+import org.jzy3d.plot3d.primitives.Scatter;
+import org.jzy3d.plot3d.rendering.canvas.Quality;
+import org.jzy3d.plot3d.rendering.view.View;
+
+import org.jzy3d.chart.controllers.mouse.camera.AWTCameraMouseController;
+
+import com.jogamp.newt.swt.NewtCanvasSWT;
+
+
+public class ScatterDemo extends AbstractAnalysis{
+	public static void main(String[] args) throws Exception {
+		AnalysisLauncher.open(new ScatterDemo());
+	}
+		
+	public void init(){
+        int size = 1000000;
+        float x;
+        float y;
+        float z;
+        float a;
+        
+        Coord3d[] points = new Coord3d[size];
+        Color[]   colors = new Color[size];
+        
+        Random r = new Random();
+        r.setSeed(0);
+        
+        
+        for(int i=0; i<size; i++){
+            x = r.nextFloat() - 0.5f;
+            y = r.nextFloat() - 0.5f;
+            z = r.nextFloat() - 0.5f;
+            points[i] = new Coord3d(x, y, z);
+            a = 0.25f;
+            colors[i] = new Color(x, y, z, a);
+        }
+        
+        
+        Scatter scatter = new Scatter(points, colors);
+        chart = AWTChartComponentFactory.chart(Quality.Fastest, "awt");
+        chart.getScene().add(scatter);
+       
+        ZoomController cont = new ZoomController();
+        chart.addController(cont);
+    }
+	
+	
+	public class ZoomController extends AWTCameraMouseController {
+		public ZoomController() {
+			super();
+		}
+		
+		public void mouseWheelMoved(MouseWheelEvent e) { 
+            if(threadController!=null) 
+                    threadController.stop(); 
+
+            float factor = 1 + (e.getWheelRotation()/10.0f); 
+            //zoom(factor); 
+
+            Scale currScale = View.current().getScale(); 
+            double range = currScale.getMax() - currScale.getMin(); 
+
+            if(range<=0) 
+                    return; 
+
+            double center = (currScale.getMax() + currScale.getMin())/2; 
+            double min   = center + (currScale.getMin()-center) * (factor); 
+            double max   = center + (currScale.getMax()-center) * (factor); 
+            Scale scale = null; 
+            if(min<max) { 
+                    scale = new org.jzy3d.maths.Scale(min, max); 
+            } 
+            else{ 
+                    if(factor<1) // forbid to have min = max if we zoom in 
+                            scale = new org.jzy3d.maths.Scale(center, center); 
+            } 
+            BoundingBox3d bounds = View.current().getBounds(); 
+            bounds.setZmin((float)scale.getMin()); 
+            bounds.setZmax((float)scale.getMax()); 
+            bounds.setYmin((float)scale.getMin()); 
+            bounds.setYmax((float)scale.getMax()); 
+            bounds.setXmin((float)scale.getMin()); 
+            bounds.setXmax((float)scale.getMax()); 
+            //View.current().setBoundManual(bounds); can't use this 
+            //View.current().updateBounds(); 
+            View.current().shoot(); 
+		}
+	}
+}
--- STORMVis/src/main/java/Test.java
+++ STORMVis/src/main/java/Test.java
@@ -1,11 +1,5 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.jzy3d.chart.Chart;
 
 public class Test {
 
