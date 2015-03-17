995e70e0056fec9e3fe01d513f86b22af1312df5
Maximilian Scheurer <maximilianscheurer@MaximiliansiMac.fritz.box>
2015-03-17 15:11:06 +0100

    pause

--- STORMVis/src/main/java/calc/Finder.java
+++ STORMVis/src/main/java/calc/Finder.java
@@ -9,6 +9,28 @@ public class Finder {
 		float[] areas = Calc.getAreas(triangles);
 		int numberOfFluorophores = (int) Math.floor(Calc.sum(areas)*bspsnm*pabs);
 		System.out.println("fluorophore  number: "+ numberOfFluorophores);
+		getRandomTriangles(areas, numberOfFluorophores);
+	}
+	
+	public static int[] getRandomTriangles(float[] areas, int nbrFluorophores) {
+		float tot = Calc.sum(areas);
+		int[] idx = new int[nbrFluorophores];
+		int[] startingindices = new int[areas.length];
+		float[] startingsum = new float[areas.length];
+		float partsum = 0;
+	    int counter = 1;
+	    int parts = 10000;
+	    
+	    for (int i = 0;i<areas.length;i++) {
+	    	partsum = partsum + areas[i];
+	    	if (partsum > tot/parts*counter) {
+	    		startingindices[counter] = i;
+	    		startingsum[counter] = partsum;
+	    		counter = counter + 1;
+	    	}
+	    }
+		
+		return null;
 	}
 	
 	
