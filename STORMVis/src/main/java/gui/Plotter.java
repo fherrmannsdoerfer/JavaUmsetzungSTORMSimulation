88e8cbe39d59898d12dbc70ecc708a6bbac8611b
Maximilian Scheurer <max.scheurer@me.com>
2015-03-23 20:39:48 +0100

    - not impl Plotter

--- STORMVis/src/main/java/gui/Plotter.java
+++ STORMVis/src/main/java/gui/Plotter.java
@@ -3,15 +3,26 @@ package gui;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.jzy3d.chart.Chart;
+import org.jzy3d.chart.factories.AWTChartComponentFactory;
+import org.jzy3d.chart.factories.IChartComponentFactory.Toolkit;
+import org.jzy3d.colors.Color;
 import org.jzy3d.maths.Coord3d;
+import org.jzy3d.plot3d.primitives.LineStrip;
+import org.jzy3d.plot3d.primitives.Point;
 import org.jzy3d.plot3d.primitives.Polygon;
+import org.jzy3d.plot3d.rendering.canvas.Quality;
 
 import gui.DataTypeDetector.DataType;
 
 public class Plotter {
 	
+	public boolean SHOWLINES;
+	
 	public DataType dataType;
 	public Object data;
+	public Quality chartQuality;
+	public int linePointNumber;
 	
 	public Plotter(Object data, DataType dataType) {
 		this.dataType = dataType;
@@ -37,6 +48,40 @@ public class Plotter {
 				data = data2;
 			}
 		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public Chart createChart() {
+		Chart chart = AWTChartComponentFactory.chart(chartQuality, Toolkit.awt.name());
+		
+		if(dataType.equals(DataType.WIMP)) {
+			List<ArrayList<Coord3d>> data2 = (List<ArrayList<Coord3d>>) data;
+			List<LineStrip> lineList = new ArrayList<LineStrip>();
+			Coord3d[] points = new Coord3d[lineParser.pointNumber];
+	        Color[] colors = new Color[lineParser.pointNumber];
+			int i = 0;
+	        for(ArrayList<Coord3d> obj : data2) {
+	        	LineStrip strip = new LineStrip();
+	    		strip.setWidth(2.f);
+	    		strip.setWireframeColor(Color.BLACK);
+	        	for(Coord3d coord : obj) {
+	        		points[i] = coord;
+	        		float a = 1.f;
+	        		colors[i] = new Color(coord.x/255.f,coord.y/255.f,coord.z/255.f,a);
+	        		if(SHOWLINES) {
+	        			strip.add(new Point(coord));
+	        		}
+	        		i++;
+	        	}
+	        	lineList.add(strip);
+	        }
+		}
+		else if (dataType.equals(DataType.NFF)) {
+			
+		}
+		
+		
+		return chart;
 	}
 	
 	
